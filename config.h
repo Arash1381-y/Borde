ee6962bd64d2698524dab610446ba4999f6fde11
halfblood <arashyadegari0402@gmail.com>
2023-07-12 02:01:10 +0330

    add config.h

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CPU_CONFIG_H
+#define CPU_CONFIG_H
+
+
+const char *INPUT_PATH = "./../../samples/";
+const char *RESULT_PATH = "./../../results/";
+
+const char BRIGHTNESS_DEFAULT = 20;
+const char SOBEL_THRESHOLD = 80;
+const double STRENGTH_RATIO = .7;
+
+
+#endif //CPU_CONFIG_H
