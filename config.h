046a0e86d7311c850c484f7da61f6a001956e183
halfblood <arashyadegari0402@gmail.com>
2023-07-12 15:48:51 +0330

    refactor consts

--- config.h
+++ config.h
@@ -2,8 +2,10 @@
 #define CPU_CONFIG_H
 
 
-const char *INPUT_PATH = "./../../samples/";
-const char *RESULT_PATH = "./../../results/";
+const char *DEFAULT_INPUT_PATH = "./../samples/";
+const char *DEFAULT_RESULT_PATH = "./../results/";
+
+const char *DEFAULT_INPUT_FILENAME = "sample.png";
 
 const char BRIGHTNESS_DEFAULT = 20;
 const char SOBEL_THRESHOLD = 80;
